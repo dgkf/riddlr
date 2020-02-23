d7cd96cfe0231894e1c9ddc8aba323d2d46c8c16
dgkf <18220321+dgkf@users.noreply.github.com>
2020-02-23 09:48:48 -0800

    cleaned up install script

--- inst/example/shiny/question_catalog/install.R
+++ inst/example/shiny/question_catalog/install.R
@@ -1,44 +1,10 @@
-if (!dir.exists('./.lib')) dir.create('./.lib')
+if_unavailable <- function(pkg, expr, envir = parent.frame())
+  if (!pkg %in% installed.packages()) invisible(eval(expr, envir = envir))
 
-new_libs <- c("./.lib", .libPaths())
-new_libs <- new_libs[!grepl("^/home", new_libs)]
-.libPaths(new_libs)
-
-installed_packages <- rownames(installed.packages())
-
-devtools::install_github("dgkf/shinyAce",
-  ref = "dev")
-
-devtools::install_github("kelkhofd/riddlr",
-  host = "github.roche.com/api/v3",
-  ref = "shiny_rewrite")
-
-if (!"shiny" %in% installed_packages || packageVersion("shiny") < package_version("1.2"))
-  install.packages("shiny")
-
-if (!"shinydashboard" %in% installed_packages || packageVersion("shinydashboard") < package_version("0.7"))
-  install.packages("shinydashboard")
-
-if (!"shinycssloaders" %in% installed_packages || packageVersion("shinycssloaders") < package_version("0.2"))
-  install.packages("shinycssloaders")
-
-if (!"markdown" %in% installed_packages || packageVersion("markdown") < package_version("0.9"))
-  install.packages("markdown")
-
-if (!"dplyr" %in% installed_packages || packageVersion("dplyr") < package_version("0.8"))
-  install.packages("dplyr")
-
-if (!"tibble" %in% installed_packages || packageVersion("tibble") < package_version("2.0"))
-  install.packages("tibble")
-
-if (!"purrr" %in% installed_packages || packageVersion("purrr") < package_version("0.3"))
-  install.packages("purrr")
-
-if (!"tidyr" %in% installed_packages || packageVersion("tidyr") < package_version("0.8"))
-  install.packages("tidyr")
-
-if (!"DT" %in% installed_packages || packageVersion("DT") < package_version("0.5"))
-  install.packages("DT")
-
-if (!"purrr" %in% installed_packages || packageVersion("purrr") < package_version("0.3"))
-  install.packages("purrr")
+if_unavailable("devtools", install.packages("devtools"))
+if_unavailable("riddlr", devtools::install_github("dgkf/riddlr"))
+if_unavailable("shinythemes", install.packages("shinythemes"))
+if_unavailable("shinydashboard", install.packages("shinydashboard"))
+if_unavailable("shinycssloaders", install.packages("shinycssloaders"))
+if_unavailable("dplyr", install.packages("dplyr"))
+if_unavailable("tidyr", install.packages("tidyr"))
